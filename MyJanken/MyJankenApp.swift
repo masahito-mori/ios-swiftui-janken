1c7108fa362c70d12a335cc29591ac43d992009f
fore447 <mmori.0426@gmail.com>
2023-08-05 22:57:35 +0900

    Initial Commit

---
+++ MyJanken/MyJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJankenApp.swift
+//  MyJanken
+//
+//  Created by 森匡人 on 2023/08/05.
+//
+
+import SwiftUI
+
+@main
+struct MyJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
